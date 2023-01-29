271322d01c60f58f9000f4d426884263ed6c539e
gaurab17 <gaurabstha001@gmail.com>
2023-01-29 11:56:22 +0545

    Landing page commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { useContext, useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import { userContext } from './main'
+import Landingpage from './Components/Landingpage'
+// import { userContext } from './main'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const user = useContext(userContext)
+  // const [count, setCount] = useState(0)
+  // const user = useContext(userContext)
 
   return (
-    <div className=' bg-slate-500'>
-      <p className='text-3xl font-bold underline'>Hello world</p>
-      <button>
-        Hello there: {user.username} from {user.address}
-      </button>
+    <div className=' h-screen bg-gradient-to-r from-[#345267] to-[#3F8F8B]'>
+      <Landingpage />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-export const userContext = createContext<{ username: string | number; address: string }>({
-  username: '',
-  address: '',
-})
+// export const userContext = createContext<{ username: string | number; address: string }>({
+//   username: '',
+//   address: '',
+// })
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <userContext.Provider value={{ username: 'Gaurab', address: 'Bangka' }}>
-      <App />
-    </userContext.Provider>
+    <App />
   </React.StrictMode>,
 )
