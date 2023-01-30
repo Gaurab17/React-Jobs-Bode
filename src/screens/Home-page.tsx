619d7017a8b310512a18e5d94b94790bb3b5cadd
gaurab17 <gaurabstha001@gmail.com>
2023-01-30 19:14:08 +0545

    Homepage created

--- src/screens/Home-page.tsx
+++ src/screens/Home-page.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 // import Container from '../Components/molecules/Container'
 import container from '../assets/svgs/container.svg'
+import Container from '../Components/molecules/Container'
 import Navbar from '../Components/molecules/Navbar'
 
 const HomePage = () => {
   return (
-    <div className='h-full'>
+    <div className='h-[565px] w-full relative '>
+      <img className=' w-full h-full object-cover absolute' src={container} alt='' />
       <Navbar />
-      <img className='h-[665px] w-full' src={container} alt='' />
+      <h1 className='text-[40px] text-[#4E4E4E] text-center pt-8'>What Are You Looking For?</h1>
+      <div className='flex flex-row items-center justify-center'>
+        <Container />
+        <Container />
+      </div>
     </div>
   )
 }
