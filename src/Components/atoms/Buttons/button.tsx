5b0212da4724c29681af8f48b5561bae325c2d31
sabin411 <sherasabin411@gmail.com>
2023-02-12 12:04:10 +0545

    feat:Styled the button from the inside.

--- src/Components/atoms/Buttons/button.tsx
+++ src/Components/atoms/Buttons/button.tsx
@@ -5,7 +5,11 @@ const Button = ({ type, text, icon, className, clickFunction, btnType }: ButtonD
   return (
     <button
       type={type}
-      className={`${btnType === 'primary' ? 'bg-[#6E877A]' : 'bg-[#345267]'} ${className}`}
+      className={`rounded-[56px] h-[75px] w-[287px] text-[27px] text-[#FFFFFF] flex justify-center items-center ${
+        btnType === 'primary' ? 'bg-[#6E877A]' : 'bg-[#345267]'
+      } ${className}
+      hover:bg-slate-700
+      `}
       onClick={clickFunction}
     >
       <span>{text}</span>
