15989ca587d31c5a9d61679e49f70dbaa9fac178
sabin411 <sherasabin411@gmail.com>
2023-02-12 12:03:33 +0545

    feat:Added extended dropshadow theme.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        '3xl': '0 20px 20px rgba(0, 0, 0, 0.25)',
+        '4xl': ['0 35px 35px rgba(0, 0, 0, 0.25)', '0 45px 65px rgba(0, 0, 0, 0.15)'],
+      },
+    },
   },
   plugins: [],
 }
